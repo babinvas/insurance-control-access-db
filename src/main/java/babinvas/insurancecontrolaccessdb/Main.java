7211eaf2f631fe0cf5f5eb1e135c8971d2ce5d8d
babinvas <babinvas@gmail.com>
2021-01-31 18:39:26 +0300

    The main class was configured to send a text email

--- src/main/java/babinvas/insurancecontrolaccessdb/Main.java
+++ src/main/java/babinvas/insurancecontrolaccessdb/Main.java
@@ -1,4 +1,16 @@
 package babinvas.insurancecontrolaccessdb;
 
+import babinvas.insurancecontrolaccessdb.services.EmailSender;
+import babinvas.insurancecontrolaccessdb.services.TextEmailSender;
+
 public class Main {
+	private static final EmailSender emailSender = new TextEmailSender("username", "password", "host", 465);
+
+	public static void main(String[] args) {
+		emailSender.setFrom("from@from.ru");
+		emailSender.setTo("to@to.ru");
+		emailSender.setCc("cc@cc.ru");
+		emailSender.setBcc("bcc@bcc.ru");
+		emailSender.send();
+	}
 }
