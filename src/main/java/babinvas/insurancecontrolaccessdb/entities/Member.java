94225b67f7ac6991d355e4a4007406981f30083b
babinvas <babinvas@gmail.com>
2021-02-04 19:54:34 +0300

    Variables were added to the member class

--- src/main/java/babinvas/insurancecontrolaccessdb/entities/Member.java
+++ src/main/java/babinvas/insurancecontrolaccessdb/entities/Member.java
@@ -1,4 +1,14 @@
 package babinvas.insurancecontrolaccessdb.entities;
 
 public class Member {
+	long id;
+
+	String surname;
+	String name;
+	String patronymic;
+	String toWhom;
+	String sexualEnding;
+
+	String mainEmail;
+	String secondEmail;
 }
