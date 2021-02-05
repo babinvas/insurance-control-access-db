d94236b424ff048d30e15e4cdc8a4cf4ed3b9889
babinvas <babinvas@gmail.com>
2021-02-05 19:29:41 +0300

    Variables were added to the employer class

--- src/main/java/babinvas/insurancecontrolaccessdb/entities/Employer.java
+++ src/main/java/babinvas/insurancecontrolaccessdb/entities/Employer.java
@@ -1,4 +1,16 @@
 package babinvas.insurancecontrolaccessdb.entities;
 
 public class Employer {
+	long id;
+
+	String fullName;
+	String shortName;
+
+	String headNamePatronymic;
+	String toHeadForm;
+	String toHeadName;
+	String headSexualEnding;
+
+	String mainEmail;
+	String secondEmail;
 }
