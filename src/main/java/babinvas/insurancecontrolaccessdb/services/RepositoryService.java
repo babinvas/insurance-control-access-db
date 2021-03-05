96cb06644b9ac6e8839a25eb2ceeb05487d1ba22
babinvas <babinvas@gmail.com>
2021-03-05 20:08:49 +0300

    The main abstract class for working with the repository was created

---
+++ src/main/java/babinvas/insurancecontrolaccessdb/services/RepositoryService.java
@@ -0,0 +1,7 @@
+package babinvas.insurancecontrolaccessdb.services;
+
+import javax.persistence.EntityManagerFactory;
+
+public abstract class RepositoryService {
+	EntityManagerFactory entityManagerFactory;
+}
