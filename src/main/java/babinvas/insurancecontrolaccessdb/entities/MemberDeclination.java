64a4e6a170ff7270e8c962899e67147b5bcf2a82
babinvas <babinvas@gmail.com>
2021-04-01 18:43:32 +0300

    The variables of the member declination class have been corrected

--- src/main/java/babinvas/insurancecontrolaccessdb/entities/MemberDeclination.java
+++ src/main/java/babinvas/insurancecontrolaccessdb/entities/MemberDeclination.java
@@ -1,16 +1,21 @@
 package babinvas.insurancecontrolaccessdb.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "11_Склонение_Членов_НКСО")
 public class MemberDeclination {
 	@Id
+	@Column(name = "Id_По_Реестру", nullable = false)
 	private String idRegistryNumber;
 
+	@Column(name = "Кому_сокр_ФИО_письмо")
 	private String abbreviationToWhom;
+
+	@Column(name = "Уважаемый_ая")
 	private String respectfulWordEnding;
 
+	@OneToOne(optional = false, mappedBy = "memberDeclination")
 	Member member;
 
 	public MemberDeclination() {
