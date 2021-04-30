0f98c0d648dc03e80102b5d3fed9b90182970047
babinvas <babinvas@gmail.com>
2021-04-30 17:46:18 +0300

    Changed the main class

--- src/main/java/babinvas/insurancecontrolaccessdb/Main.java
+++ src/main/java/babinvas/insurancecontrolaccessdb/Main.java
@@ -1,8 +1,6 @@
 package babinvas.insurancecontrolaccessdb;
 
-import babinvas.insurancecontrolaccessdb.entities.Member;
-import babinvas.insurancecontrolaccessdb.entities.MemberDeclination;
-import babinvas.insurancecontrolaccessdb.entities.MemberInsurance;
+import babinvas.insurancecontrolaccessdb.entities.*;
 import babinvas.insurancecontrolaccessdb.services.email.EmailSendingService;
 import babinvas.insurancecontrolaccessdb.services.email.TextEmailSendingService;
 import babinvas.insurancecontrolaccessdb.services.repository.MemberRepositoryService;
@@ -16,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-//TODO Реобходимо удалить все ненужные методы перед запуском
+//TODO Необходимо удалить все ненужные методы перед запуском
 
 public class Main {
 	private static final EmailSendingService emailSendingService = new TextEmailSendingService("username", "password", "host", 465);
@@ -37,18 +35,18 @@ public class Main {
 
 		entityTransaction = entityManager.getTransaction();
 
-		//TODO Реобходимо удалить запуском
+		//TODO Необходимо удалить запуском перед запуском
 		// createEntities();
 		// readEntities();
 		// updateEntities();
 		// deleteEntities();
 
 		MemberRepositoryService memberRepositoryService = new MemberRepositoryService(entityManagerFactory);
-		Date date = getDate(new Date());
+		// Date date = getDate(new Date());
 
-		//TODO Реобходимо удалить запуском
+		//TODO Необходимо удалить запуском перед запуском
 		// Date date = date1.
-		// Date date = getDate("03.05.2021");
+		Date date = getDate("03.05.2021");
 
 		List<Member> memberList = memberRepositoryService.getByInsurance(date);
 
@@ -56,23 +54,31 @@ public class Main {
 			System.out.println(member.getIdRegistryNumber() + " " + member.getSurname());
 		}
 
+		//TODO Необходимо удалить запуском перед запуском
+		Company company = readCompany("00001", "1");
+
 		entityManager.getEntityManagerFactory().close();
 		entityManager.close();
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
+	private static Company readCompany(String idRegistryNumber, String idCompany) {
+		return entityManager.find(Company.class, new CompanyKey(idRegistryNumber, idCompany));
+	}
+
+	//TODO Необходимо удалить запуском перед запуском
 	private static Member getMember(String idRegistryNumber, String surname, String name, String patronymic, String mainEmail, String secondEmail) {
 		Member member = new Member(idRegistryNumber);
 		member.setSurname(surname);
 		member.setName(name);
 		member.setPatronymic(patronymic);
 		member.setMainEmail(mainEmail);
-		member.secondEmail(secondEmail);
+		member.setSecondEmail(secondEmail);
 
 		return member;
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static MemberDeclination getMemberDeclination(Member member, String abbreviationToWhom, String respectfulWordEnding) {
 		MemberDeclination memberDeclination = new MemberDeclination(member);
 		memberDeclination.setAbbreviationToWhom(abbreviationToWhom);
@@ -81,7 +87,7 @@ public class Main {
 		return memberDeclination;
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static MemberInsurance getMemberInsurance(Member member, String expirationDate) {
 		MemberInsurance memberInsurance = new MemberInsurance(member);
 
@@ -91,7 +97,7 @@ public class Main {
 		return memberInsurance;
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static Date getDate(String date) {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
@@ -104,7 +110,7 @@ public class Main {
 		return null;
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static Date getDate(Date date) {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
@@ -118,7 +124,7 @@ public class Main {
 		return null;
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static void createEntities() {
 		entityTransaction.begin();
 
@@ -163,14 +169,14 @@ public class Main {
 		}
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static void readEntities() {
 		Member member4 = entityManager.find(Member.class, "00001");
 		MemberDeclination memberDeclination4 = entityManager.find(MemberDeclination.class, "00002");
 		MemberInsurance memberInsurance4 = entityManager.find(MemberInsurance.class, "00003");
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static void updateEntities() {
 		entityTransaction.begin();
 
@@ -184,7 +190,7 @@ public class Main {
 		}
 	}
 
-	//TODO Реобходимо удалить запуском
+	//TODO Необходимо удалить запуском перед запуском
 	private static void deleteEntities() {
 		entityTransaction.begin();
 
