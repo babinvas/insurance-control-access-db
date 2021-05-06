37c97f433777584734bbf8da759862d09c12cbfa
babinvas <babinvas@gmail.com>
2021-05-06 21:26:04 +0300

    Added several methods to the test class and refactored the class

--- src/test/java/babinvas/insurancecontrolaccessdb/services/repository/MemberRepositoryServiceTest.java
+++ src/test/java/babinvas/insurancecontrolaccessdb/services/repository/MemberRepositoryServiceTest.java
@@ -1,8 +1,7 @@
 package babinvas.insurancecontrolaccessdb.services.repository;
 
 import babinvas.insurancecontrolaccessdb.entities.Member;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,55 +15,64 @@ import java.util.Date;
 import java.util.List;
 
 class MemberRepositoryServiceTest {
-	EntityManagerFactory entityManagerFactory;
+	private static EntityManagerFactory entityManagerFactory;
+	private EntityManager entityManager;
 
 	@Test
 	void getByInsurance_ShouldReturnTrueForIdenticalLists() {
-		entityManagerFactory = Persistence.createEntityManagerFactory("nkso-members-and-employers");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-
 		// Gets the expiration date of the member (President)
-		Member member = entityManager.find(Member.class, "00005");
-		Date expirationDate = member.getMemberInsurance().getExpirationDate();
+		Date expirationDate = getDate("00005");
 
-		EntityTransaction entityTransaction = entityManager.getTransaction();
-		entityTransaction.begin();
-
-		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-
-		CriteriaQuery<Member> query = criteriaBuilder.createQuery(Member.class);
-		Root<Member> root = query.from(Member.class);
-
-		query.select(root);
-
-		query.where(
-				criteriaBuilder.equal(
-						root.
-								get("memberInsurance").
-								get("expirationDate"), expirationDate));
-
-		List<Member> expectedMembers = entityManager.createQuery(query).getResultList();
+		List<Member> expectedMembers = getMembersList(expirationDate);
 
 		MemberRepositoryService memberRepositoryService = new MemberRepositoryService(entityManagerFactory);
 		List<Member> actualMembers = memberRepositoryService.getByInsurance(expirationDate);
 
 		Assertions.assertArrayEquals(expectedMembers.toArray(), actualMembers.toArray());
-
-		entityTransaction.commit();
-		entityManager.close();
 	}
 
 	@Test
 	void getByInsurance_ShouldReturnFalseForIdenticalLists() {
-		entityManagerFactory = Persistence.createEntityManagerFactory("nkso-members-and-employers");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-
 		// Gets the expiration date of the member (President)
-		Member member = entityManager.find(Member.class, "00005");
-		Date expirationDate = member.getMemberInsurance().getExpirationDate();
+		Date expirationDate = getDate("00005");
 
+		List<Member> expectedMembers = getMembersList(expirationDate);
+
+		expirationDate = new Date(expirationDate.getTime() + (1000 * 60 * 60 * 24));
+
+		MemberRepositoryService memberRepositoryService = new MemberRepositoryService(entityManagerFactory);
+		List<Member> actualMembers = memberRepositoryService.getByInsurance(expirationDate);
+
+		Assertions.assertNotEquals(expectedMembers, actualMembers);
+	}
+
+	@BeforeAll
+	static void setup() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("nkso-members-and-employers");
+	}
+
+	@AfterAll
+	static void done() {
+		entityManagerFactory.close();
+	}
+
+	@BeforeEach
+	void init() {
+		entityManager = entityManagerFactory.createEntityManager();
+	}
+
+	@AfterEach
+	void close() {
+		entityManager.close();
+	}
+
+	private Date getDate(String memberId) {
+		Member member = entityManager.find(Member.class, memberId);
+		return member.getMemberInsurance().getExpirationDate();
+	}
+
+	private List<Member> getMembersList(Date date) {
 		EntityTransaction entityTransaction = entityManager.getTransaction();
-		entityTransaction.begin();
 
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 
@@ -77,19 +85,8 @@ class MemberRepositoryServiceTest {
 				criteriaBuilder.equal(
 						root.
 								get("memberInsurance").
-								get("expirationDate"), expirationDate));
+								get("expirationDate"), date));
 
-		List<Member> expectedMembers = entityManager.createQuery(query).getResultList();
-
-		member = entityManager.find(Member.class, "00001");
-		expirationDate = member.getMemberInsurance().getExpirationDate();
-
-		MemberRepositoryService memberRepositoryService = new MemberRepositoryService(entityManagerFactory);
-		List<Member> actualMembers = memberRepositoryService.getByInsurance(expirationDate);
-
-		Assertions.assertNotEquals(expectedMembers, actualMembers);
-
-		entityTransaction.commit();
-		entityManager.close();
+		return entityManager.createQuery(query).getResultList();
 	}
 }
