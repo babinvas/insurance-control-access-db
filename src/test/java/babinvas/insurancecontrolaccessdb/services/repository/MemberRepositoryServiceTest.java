bc71da5d27fdd0bea7ac5a3b71dc21c0ae0b7131
babinvas <babinvas@gmail.com>
2021-05-07 18:33:54 +0300

    Refactored the test class and changed some information in the database

--- src/test/java/babinvas/insurancecontrolaccessdb/services/repository/MemberRepositoryServiceTest.java
+++ src/test/java/babinvas/insurancecontrolaccessdb/services/repository/MemberRepositoryServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -72,8 +71,6 @@ class MemberRepositoryServiceTest {
 	}
 
 	private List<Member> getMembersList(Date date) {
-		EntityTransaction entityTransaction = entityManager.getTransaction();
-
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 
 		CriteriaQuery<Member> query = criteriaBuilder.createQuery(Member.class);
