7ff542877f9f1e72fc3aa2c8d7c5c28029a75337
babinvas <babinvas@gmail.com>
2021-06-15 18:29:57 +0300

    Created a test class with methods for the email class

---
+++ src/test/java/babinvas/insurancecontrolaccessdb/services/email/TextEmailSendingServiceTest.java
@@ -0,0 +1,16 @@
+package babinvas.insurancecontrolaccessdb.services.email;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextEmailSendingServiceTest {
+
+	@Test
+	void send() {
+	}
+
+	@Test
+	void getSession() {
+	}
+}
