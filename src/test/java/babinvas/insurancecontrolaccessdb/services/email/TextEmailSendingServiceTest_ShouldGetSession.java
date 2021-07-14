47b83e46380cbd2e467c64a64606005217424ff6
babinvas <babinvas@gmail.com>
2021-07-14 18:40:34 +0300

    Implemented the method for checking the get of the session

--- src/test/java/babinvas/insurancecontrolaccessdb/services/email/TextEmailSendingServiceTest_ShouldGetSession.java
+++ src/test/java/babinvas/insurancecontrolaccessdb/services/email/TextEmailSendingServiceTest_ShouldGetSession.java
@@ -1,30 +1,39 @@
 package babinvas.insurancecontrolaccessdb.services.email;
 
 import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.mail.*;
+import java.util.Properties;
 
-import java.io.IOException;
-
-class TextEmailSendingServiceTest {
-	private TextEmailSendingService textEmailSendingService;
-	private GreenMail greenMail;
-
-	@BeforeEach
-	private void startGreenMail() {
-		textEmailSendingService = new TextEmailSendingService();
-		greenMail = new GreenMail(ServerSetupTest.ALL);
+class TextEmailSendingServiceTest_ShouldGetSession {
+	@Test
+	public void getSession() {
+		GreenMail greenMail = new GreenMail(ServerSetupTest.ALL);
 		greenMail.start();
-	}
+		TextEmailSendingService textEmailSendingService = new TextEmailSendingService();
 
-	@AfterEach
-	private void stopGreenMail() {
+		final String expectedUsername = "from@test.com";
+		final String expectedPassword = "password";
+		String expectedHost = "127.0.0.1";
+		int expectedPort = 3025;
+
+		Session session = textEmailSendingService.getSession(expectedUsername, expectedPassword, expectedHost, expectedPort);
+
+		Properties properties = session.getProperties();
+
+		String actualHost = properties.getProperty("mail.smtp.host");
+		String actualSsl = properties.getProperty("mail.smtp.ssl.enable");
+		String actualAuth = properties.getProperty("mail.smtp.auth");
+		String actualDebug = properties.getProperty("mail.debug");
+
+		Assertions.assertEquals(expectedHost, actualHost);
+		Assertions.assertEquals("true", actualSsl);
+		Assertions.assertEquals("true", actualAuth);
+		Assertions.assertEquals("true", actualDebug);
+
 		greenMail.stop();
 	}
 }
